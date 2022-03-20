1cb91059cd3b49f0cd2d45b4903560699204b432
vova7878 <vova20017878@gmail.com>
2022-03-20 14:02:39 +0700

    New Implementation

--- Tree16.hpp
+++ Tree16.hpp
@@ -3,218 +3,222 @@
 
 #include "jtypes.hpp"
 #include <stdexcept>
-#include <cstring>
 
-template<typename T, unsigned int level>
-class Tree16 {
-    using data_type = Tree16<T, level - 1 >;
+u2 numberOfTrailingZeros(u2 i) {
+    // HD, Figure 5-14
+    if (i == 0) return 16;
+    u2 y;
+    u1 n = 15;
+    y = i << 8;
+    if (y) {
+        n -= 8;
+        i = y;
+    }
+    y = i << 4;
+    if (y) {
+        n -= 4;
+        i = y;
+    }
+    y = i << 2;
+    if (y) {
+        n -= 2;
+        i = y;
+    }
+    y = i << 1;
+    return n - (y >> 15);
+}
+
+inline bool getFlag(u2 data, u4 index) {
+    return data & 1 << index;
+}
+
+inline void setFlag(u2& data, u4 index, bool value) {
+    data = value ? data | 1 << index : data & ~(1 << index);
+}
+
+template<typename T, T nullValue>
+class Tree16;
+
+template<typename T, T nullValue, unsigned int level>
+class Tree16_Impl {
+    using data_type = Tree16_Impl<T, nullValue, level - 1 >;
+public:
+
+    Tree16_Impl() = default;
+    ~Tree16_Impl() = default;
+
+    inline bool isFull() const {
+        u2 tmp = ~full;
+        return !tmp;
+    }
+
+    inline bool isEmpty() const {
+        return !full;
+    }
+
+    u4 put(T value) {
+        u4 index = numberOfTrailingZeros(~full);
+        if (index == 16) {
+            throw std::runtime_error("Unable to put object, tree is full");
+        }
+        if (!getFlag(exists, index)) {
+            if (value == nullValue) {
+                return index;
+            }
+            data[index].init();
+            int out = data[index].put(value);
+            setFlag(exists, index, true);
+            setFlag(full, index, false);
+            return (out << 4) | index;
+        }
+        int out = data[index].put(value);
+        setFlag(full, index, data[index].isFull());
+        return (out << 4) | index;
+    }
+
+    T put(T value, u4 position) {
+        u4 index = position & 0b1111;
+        if (!getFlag(exists, index)) {
+            if (value == nullValue) {
+                return nullValue;
+            }
+            data[index].init();
+            T out = data[index].put(value, position >> 4);
+            setFlag(exists, index, true);
+            setFlag(full, index, false);
+            return out;
+        }
+        T out = data[index].put(value, position >> 4);
+        if (data[index].isEmpty()) {
+            data[index].exit();
+            setFlag(exists, index, false);
+            setFlag(full, index, false);
+            return out;
+        }
+        setFlag(full, index, data[index].isFull());
+        return out;
+    }
+
+    T get(u4 position) const {
+        u4 index = position & 0b1111;
+        return getFlag(exists, index) ? data[index].get(position >> 4) : nullValue;
+    }
+
+    inline T remove(u4 position) {
+        return put(nullValue, position);
+    }
+
+private:
+
+    void init() {
+        full = 0;
+        exists = 0;
+        data = reinterpret_cast<data_type*> (malloc(sizeof (data_type) * 16));
+        if (data == nullptr) {
+            throw std::bad_alloc();
+        }
+    }
+
+    void exit() {
+        for (int i = 0; i < 16; i++) {
+            if (getFlag(exists, i)) {
+                data[i].exit();
+            }
+        }
+        if (data != nullptr) {
+            free(data);
+        }
+    }
+
+    Tree16_Impl(const Tree16_Impl& orig);
+
+    data_type *data;
+    u2 full;
+    u2 exists;
+
+    friend Tree16_Impl<T, nullValue, level + 1 >;
+    friend Tree16<T, nullValue>;
+};
+
+template<typename T, T nullValue>
+class Tree16_Impl<T, nullValue, 0> {
+public:
+
+    Tree16_Impl() = default;
+    ~Tree16_Impl() = default;
+
+    inline bool isFull() const {
+        u2 tmp = ~full;
+        return !tmp;
+    }
+
+    inline bool isEmpty() const {
+        return !full;
+    }
+
+    u4 put(T value) {
+        u4 index = numberOfTrailingZeros(~full);
+        if (index == 16) {
+            throw std::runtime_error("Unable to put object, tree is full");
+        }
+        data[index] = value;
+        setFlag(full, index, value != nullValue);
+        return index;
+    }
+
+    T put(T value, u4 index) {
+        T out = get(index);
+        data[index] = value;
+        setFlag(full, index, value != nullValue);
+        return out;
+    }
+
+    inline T get(u4 index) const {
+        return getFlag(full, index) ? data[index] : nullValue;
+    }
+
+    inline T remove(u4 index) {
+        return put(nullValue, index);
+    }
+
+private:
+
+    void init() {
+        full = 0;
+        data = reinterpret_cast<T*> (malloc(sizeof (T) * 16));
+        if (data == nullptr) {
+            throw std::bad_alloc();
+        }
+    }
+
+    void exit() {
+        if (data != nullptr) {
+            free(data);
+        }
+    }
+
+    Tree16_Impl(const Tree16_Impl& orig);
+
+    T *data;
+    u2 full;
+
+    friend Tree16_Impl<T, nullValue, 1>;
+};
+
+template<typename T, T nullValue>
+class Tree16 : public Tree16_Impl<T, nullValue, 7> {
 public:
 
     Tree16() {
-        memset(data, 0, sizeof (data));
+        Tree16_Impl<T, nullValue, 7>::init();
     }
 
     ~Tree16() {
-        for (int i = 0; i < 16; i++) {
-            if (data[i] != nullptr) {
-                delete data[i];
-            }
-        }
+        Tree16_Impl<T, nullValue, 7>::exit();
     }
 
-    bool isFull() {
-        return full;
-    }
-
-    bool isEmpty() {
-        bool empty = true;
-        for (int i = 0; i < 16; i++) {
-            if (data[i] != nullptr) {
-                empty = false;
-                break;
-            }
-        }
-        return empty;
-    }
-
-    u4 put(T* value) {
-        if (value == nullptr) {
-            throw std::runtime_error("Unable to put null pointer");
-        }
-        if (isFull()) {
-            throw std::runtime_error("Unable to put object, tree is full");
-        }
-        u4 out = 0;
-        bool tmp_full = true;
-        bool set = false;
-        for (u4 i = 0; i < 16; i++) {
-            data_type* child = data[i];
-            if (child == nullptr || !child->isFull()) {
-                if (set) {
-                    tmp_full = false;
-                    break;
-                } else {
-                    if (child == nullptr) {
-                        child = new data_type();
-                        data[i] = child;
-                    }
-                    u4 tmp_position = child->put(value);
-                    out = (tmp_position << 4) | i;
-                    if (!child->isFull()) {
-                        tmp_full = false;
-                        break;
-                    }
-                    set = true;
-                }
-            }
-        }
-        full = tmp_full;
-        return out;
-    }
-
-    T* put(T* value, u4 position) {
-        u4 index = position & 0b1111;
-        data_type* child = data[index];
-        if (child == nullptr) {
-            if (value != nullptr) {
-                child = new data_type();
-                data[index] = child;
-                child->put(value, position >> 4);
-            }
-            return nullptr;
-        }
-        T* out = child->put(value, position >> 4);
-        if (value == nullptr) {
-            full = false;
-            return out;
-        }
-        bool tmp_full = true;
-        for (int i = 0; i < 16; i++) {
-            if (data[i] == nullptr || !data[i]->isFull()) {
-                full = false;
-                break;
-            }
-        }
-        full = tmp_full;
-        return out;
-    }
-
-    T* get(u4 position) {
-        u4 index = position & 0b1111;
-        data_type* child = data[index];
-        return child == nullptr ? nullptr : child->get(position >> 4);
-    }
-
-    T* remove(u4 position) {
-        u4 index = position & 0b1111;
-        data_type* child = data[index];
-        T* out;
-        if (child == nullptr) {
-            out = nullptr;
-        } else {
-            out = child->remove(position >> 4);
-            if (child->isEmpty()) {
-                delete child;
-                data[index] = nullptr;
-            }
-        }
-        if (out != nullptr && full) {
-            full = false;
-        }
-        return out;
-    }
 private:
-    bool full = false;
-    data_type * data[16];
-    Tree16(const Tree16& orig);
-};
 
-template<typename T>
-class Tree16<T, 0> {
-public:
-
-    Tree16() {
-        memset(data, 0, sizeof (data));
-    }
-    ~Tree16() = default;
-
-    bool isFull() const {
-        return full;
-    }
-
-    bool isEmpty() const {
-        bool empty = true;
-        for (int i = 0; i < 16; i++) {
-            if (data[i] != nullptr) {
-                empty = false;
-                break;
-            }
-        }
-        return empty;
-    }
-
-    u4 put(T* value) {
-        if (value == nullptr) {
-            throw std::runtime_error("Unable to put null pointer");
-        }
-        if (isFull()) {
-            throw std::runtime_error("Unable to put object, tree is full");
-        }
-        u4 out = 0;
-        bool tmp_full = true;
-        bool set = false;
-        for (u4 i = 0; i < 16; i++) {
-            if (data[i] == nullptr) {
-                if (set) {
-                    tmp_full = false;
-                    break;
-                } else {
-                    data[i] = value;
-                    out = i;
-                    set = true;
-                }
-            }
-        }
-        full = tmp_full;
-        return out;
-    }
-
-    T* put(T* value, u4 position) {
-        u4 index = position & 0b1111;
-        T* out = data[index];
-        data[index] = value;
-        if (value == nullptr) {
-            full = false;
-            return out;
-        }
-        bool tmp_full = true;
-        for (int i = 0; i < 16; i++) {
-            if (data[i] == nullptr) {
-                tmp_full = false;
-                break;
-            }
-        }
-        full = tmp_full;
-        return out;
-    }
-
-    T* get(u4 position) {
-        u4 index = position & 0b1111;
-        return data[index];
-    }
-
-    T* remove(u4 position) {
-        u4 index = position & 0b1111;
-        T* out = data[index];
-        data[index] = nullptr;
-        if (out != nullptr && full) {
-            full = false;
-        }
-        return out;
-    }
-private:
-    bool full = false;
-    T* data[16];
     Tree16(const Tree16& orig);
 };
 
